933c77b4487db70de6ffee20c0735bd8e6ca59ca
Gabriel Eduardo P.W. <159468605+GabehEPW@users.noreply.github.com>
2026-05-04 15:05:45 +0000

    tentando  resolver erros

--- src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/messaging/config/SqsMessagingConfig.java
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/messaging/config/SqsMessagingConfig.java
@@ -2,8 +2,10 @@ package com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.mes
 
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("aws")
 @EnableSqs
 public class SqsMessagingConfig {
 }
--- src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/messaging/pedidos/listener/PedidoSqsAdapter.java
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/messaging/pedidos/listener/PedidoSqsAdapter.java
@@ -8,9 +8,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("aws")
 public class PedidoSqsAdapter {
 
     private static final Logger log = LoggerFactory.getLogger(PedidoSqsAdapter.class);
